ef2ca127cb20179fbb81af018f639645efebef3b
Nam Dinh <nam.dt247@gmail.com>
2021-01-15 19:32:46 +0700

    first commit

---
+++ CrawlerLink/DbContextCrawler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlerLink
+{
+    class DbContextCrawler : DbContext
+    {
+        public DbContextCrawler() : base("DbContextCrawler")
+        {
+
+        }
+        public DbSet<Article> Articles { get; set; }
+    }
+}
